49cd5366336e920b5b885fac3a54d98170f34027
Gavinthompson23 <mavericktakoda@gmail.com>
2023-08-04 22:41:29 -0700

    fixed flake8 errors

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,8 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 # ... place you code here to LIST accounts ...
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -81,7 +83,7 @@ def list_accounts():
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
-    this endpoint will read an account based on the account_id 
+    this endpoint will read an account based on the account_id
     """
     app.logger.info("Request to read an Account with id: $s", account_id)
 
@@ -90,6 +92,7 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found")
 
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -108,6 +111,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # DELETE AN ACCOUNT
